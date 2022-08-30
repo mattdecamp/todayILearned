a00d3dffa5120cd3058bf29a414467e75207122d
Matt DeCamp <matthewdecamp@gmail.com>
2022-08-30 15:02:37 -0400

    edit, post, and delete authentication checkpoints

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,3 +17,29 @@ exports.logout = (req, res) => {
   res.redirect("/");
   
 };
+
+exports.authEdit = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next(); // user is logged in
+    return
+  }
+  req.flash('error', "Whoops! You must be logged in to edit your account.")
+  res.redirect('/login')
+}
+exports.authPost = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next(); // user is logged in
+    return
+  }
+  req.flash('error', "Whoops! You must be logged in to create a post.")
+  res.redirect('/login')
+}
+
+exports.authDelete = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next(); // user is logged in
+    return
+  }
+  req.flash('error', "Whoops! You must be logged in to delete a post.")
+  res.redirect('/login')
+}
