fcd5d866afc0044406b02149e0d44c45d3f57d17
Matt DeCamp <matt@mattdecamp.com>
2023-01-24 07:29:34 -0500

    rework login flash messaging

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,23 +1,5 @@
 const passport = require("passport");
 
-exports.login = passport.authenticate("local", {
-  failureRedirect: "/login",
-  failureFlash: 'Failed Login!',
-  successRedirect: "/",
-  successFlash: 'You are now logged in!',
-});
-
-exports.logout = (req, res) => {
-  req.logout(function (err) {
-    if (err) { return next(err); }
-    // flash is not working
-  });
-  
-  req.flash("success", "You are now logged out!");
-  res.redirect("/");
-  
-};
-
 exports.authEdit = (req, res, next) => {
   if (req.isAuthenticated()) {
     next(); // user is logged in
@@ -26,6 +8,7 @@ exports.authEdit = (req, res, next) => {
   req.flash('error', "Whoops! You must be logged in to edit your account.")
   res.redirect('/login')
 }
+
 exports.authPost = (req, res, next) => {
   if (req.isAuthenticated()) {
     next(); // user is logged in
