5b8c49c5200f9cf79f486dd724ac6e55d7067f42
Matt DeCamp <matt@mattdecamp.com>
2023-01-24 07:30:43 -0500

    remove unused code

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,17 @@ const feedController = require("../controllers/feedController");
 const userController = require("../controllers/userController");
 const authController = require("../controllers/authController");
 const { catchErrors } = require("../handlers/errorHandlers");
+const passport = require("passport");
 
 /* GET home page. */
-// router.get("/", function (req, res, next) {
-//   res.render("index", { title: "Today I Learned..." });
-// });
 router.get("/", catchErrors(feedController.getFeed));
 
-router.get("/:id/delete", authController.authDelete, catchErrors(feedController.deleteFeedItem));
+router.get(
+  "/:id/delete",
+  authController.authDelete,
+  catchErrors(feedController.deleteFeedItem)
+);
 
-// router.get('/', feedController.addFeedItem);
 router.post(
   "/add",
   authController.authPost,
@@ -25,7 +26,10 @@ router.post(
 router.get("/login", userController.loginForm);
 
 // login user
-router.post("/login", authController.login);
+router.post("/login", passport.authenticate("local"), function (req, res) {
+  req.flash("success", "You are now logged in!");
+  res.redirect("/");
+});
 
 // render register page
 router.get("/register", userController.registerForm);
@@ -33,15 +37,22 @@ router.get("/register", userController.registerForm);
 // register user
 router.post(
   "/register",
-
   userController.validationBodyRules,
   userController.validation,
   userController.registerUser,
-  authController.login
+  passport.authenticate('local'), function(req, res) {
+    req.flash("success", "You are now registered!");
+      res.redirect('/');
+  }
 );
 
 // logout user
-router.get("/logout", authController.logout);
+router.get("/logout", function(req, res) {
+  req.logout(function (err) {
+    if (err) { return next(err); }
+  });
+  res.redirect("/register");
+});
 
 router.get("/account", authController.authEdit, userController.editAccount);
 router.post("/account", catchErrors(userController.updateAccount));
