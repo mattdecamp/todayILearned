21058e98a570edcdb80c47f167067f9bb11511ef
Matt DeCamp <matt@mattdecamp.com>
2023-07-24 19:52:19 -0400

    new approach to passport

--- handlers/passport.js
+++ handlers/passport.js
@@ -1,9 +1,10 @@
 const passport = require("passport");
-const LocalStrategy = require('passport-local').Strategy;
+const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
 
-passport.use(User.createStrategy());
+// passport.use(User.createStrategy());
+passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
